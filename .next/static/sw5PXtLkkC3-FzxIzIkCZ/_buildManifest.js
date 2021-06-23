557fafcca58a1623104828ac3235056b7a9c1553
Renan Carvalho <renan@interprocess.com.br>
2021-06-23 11:47:18 -0300

    chore: Corrige arquivos

--- .next/static/sw5PXtLkkC3-FzxIzIkCZ/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-da326c7a7dc841a1efb0.js"],"/_error":["static/chunks/pages/_error-9faf4177fb4e528b4124.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
