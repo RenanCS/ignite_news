c3a085cc36293399444289bc568db2f1d2afb6d5
Renan Carvalho <renan@interprocess.com.br>
2021-06-30 18:00:51 -0300

    feature(Auth): Adiciona autenticação com github

--- src/components/SignInButton/index.tsx
+++ src/components/SignInButton/index.tsx
@@ -1,23 +1,25 @@
 import styles from './styles.module.scss';
 import { FaGithub } from 'react-icons/fa';
-import {FiX} from 'react-icons/fi';
+import { FiX } from 'react-icons/fi';
+import { signIn, signOut, useSession } from 'next-auth/client';
 
-export function SignInButton () {
-    const isUserLoggedIn = true;
-
-    return isUserLoggedIn? (
+export function SignInButton() {
+    const [session] = useSession();
+    return session ? (
         <button
-        className={styles.signInButton}
-        type="button">
-            <FaGithub color="#04d361"/>
-            Renan Carvalho
-            <FiX color="#737380" className={styles.closeIcon}/>
+            className={styles.signInButton}
+            type="button">
+            <FaGithub color="#04d361"
+            onClick={() => signOut()} />
+            {session.user.name}
+            <FiX color="#737380" className={styles.closeIcon} />
         </button>
     ) : (
         <button
-        className={styles.signInButton}
-        type="button">
-            <FaGithub color="#eba417"/>
+            className={styles.signInButton}
+            type="button"
+            onClick={() => signIn('github')}>
+            <FaGithub color="#eba417" />
             Sign in with Github
         </button>
     );
