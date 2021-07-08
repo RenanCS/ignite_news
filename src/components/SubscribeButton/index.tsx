a3713b1a41bc42e3e70d6ad519b9f899c7f97130
Renan Carvalho <renan@interprocess.com.br>
2021-07-08 15:34:45 -0300

    feat: Adiciona pagamentos vinculados ao stripe

--- src/components/SubscribeButton/index.tsx
+++ src/components/SubscribeButton/index.tsx
@@ -1,15 +1,39 @@
+import { signIn, useSession } from 'next-auth/client';
+import { api } from 'services/api';
+import { getStripeJS } from 'services/stripe-js';
 import { SubscribeButtonProps } from './interface';
 import styles from './styles.module.scss';
 
-export function SubscribeButton({priceId}: SubscribeButtonProps) {
-    return (
-        <button
-            type="button"
-            className={styles.subscribeButton}
-        >
-            Subscribe now
-        </button>
+export function SubscribeButton({ priceId }: SubscribeButtonProps) {
+  const [session] = useSession();
+
+  async function handleSubscribe() {
+    if (!session) {
+      signIn('github');
+      return;
+    }
+
+    try {
+      const response = await api.post('/subscribe');
+      const { sessionId } = response.data;
+      const stripe = await getStripeJS();
+
+      await stripe.redirectToCheckout({sessionId: sessionId});
+    } catch (err) {
+      alert(err.message);
+    }
+
+  }
+
+  return (
+    <button
+      type="button"
+      className={styles.subscribeButton}
+      onClick={handleSubscribe}
+    >
+      Subscribe now
+    </button>
 
 
-    );
+  );
 }
