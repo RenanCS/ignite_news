34033f7fe41709ec907363f1a8db9c6894288979
Renan Carvalho <renan@interprocess.com.br>
2021-07-08 16:33:42 -0300

    build: Teste de post api

--- src/pages/api/subscribe.ts
+++ src/pages/api/subscribe.ts
@@ -15,58 +15,66 @@ type User = {
 
 const checkoutSession = async (request: NextApiRequest, response: NextApiResponse) => {
   console.log(request);
-  response.setHeader('Cache-Control','s-maxage=10, stale-while-revalidate');
+  response.setHeader('Cache-Control', 's-maxage=10, stale-while-revalidate');
 
   if (request.method === 'POST') {
-    const session = await getSession({ req: request });
+    try {
 
-    //buscar usuário cadastrado no faunadb
-    const user = await fauna.query<User>(
-      q.Get(
-        q.Match(
-          q.Index('user_by_email'),
-          q.Casefold(session.user.email)
+
+      const session = await getSession({ req: request });
+
+      //buscar usuário cadastrado no faunadb
+      const user = await fauna.query<User>(
+        q.Get(
+          q.Match(
+            q.Index('user_by_email'),
+            q.Casefold(session.user.email)
+          )
         )
-      )
-    );
+      );
 
-    let customerId = user.data.stripe_customer_id;
+      let customerId = user.data.stripe_customer_id;
 
-    if (!customerId) {
+      if (!customerId) {
 
-      // criar o usuário no stripe
-      const stripeCustomer = await stripe.customers.create({
-        email: session.user.email
+        // criar o usuário no stripe
+        const stripeCustomer = await stripe.customers.create({
+          email: session.user.email
+        });
+
+        // salvar a referência do fauna, vinculado ao usuário no faunadb
+        await fauna.query(
+          q.Update(
+            q.Ref(q.Collection('users'), user.ref.id),
+            {
+              data: {
+                stripe_customer_id: stripeCustomer.id,
+              }
+            }
+          )
+        )
+        customerId = stripeCustomer.id;
+      }
+
+      const checkoutStripeSession = await stripe.checkout.sessions.create({
+        customer: customerId,
+        payment_method_types: ['card'],
+        billing_address_collection: 'required',
+        line_items: [
+          { price: 'price_1J89AHDEreH9cN5raVkTPhN8', quantity: 1 }
+        ],
+        mode: 'subscription',
+        allow_promotion_codes: true,
+        success_url: process.env.STRIPE_SUCESS_URL,
+        cancel_url: process.env.STRIPE_CANCEL_URL
       });
 
-      // salvar a referência do fauna, vinculado ao usuário no faunadb
-      await fauna.query(
-        q.Update(
-          q.Ref(q.Collection('users'), user.ref.id),
-          {
-            data: {
-              stripe_customer_id: stripeCustomer.id,
-            }
-          }
-        )
-      )
-      customerId = stripeCustomer.id;
+      return response.status(200).json({ sessionId: checkoutStripeSession.id });
+
+    } catch (err) {
+      console.log(err)
+      alert(err.message);
     }
-
-    const checkoutStripeSession = await stripe.checkout.sessions.create({
-      customer: customerId,
-      payment_method_types: ['card'],
-      billing_address_collection: 'required',
-      line_items: [
-        { price: 'price_1J89AHDEreH9cN5raVkTPhN8', quantity: 1 }
-      ],
-      mode: 'subscription',
-      allow_promotion_codes: true,
-      success_url: process.env.STRIPE_SUCESS_URL,
-      cancel_url: process.env.STRIPE_CANCEL_URL
-    });
-
-    return response.status(200).json({ sessionId: checkoutStripeSession.id });
   } else {
     response.setHeader('Allow', 'POST');
     return response.status(405).end('Somente é permitido POST');
